af92a7cf74162beff6b3c85141c2da5eb9d9e465
Olivier Gauthier <ogauthier@betomorrow.com>
2018-01-04 16:46:27 +0100

    First commit

---
+++ Sample/Page1.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Sample {
+    public partial class Page1 : ContentPage {
+        
+        public Page1() {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+            BindingContext = this;
+
+            Button.Command = new Command((obj) => {
+                Navigation.PushAsync(new Page2());
+            });
+        }
+    }
+}
---
+++ Sample/Page2.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Sample {
+    public partial class Page2 : ContentPage {
+        
+        public Page2() {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+            BindingContext = this;
+
+            Button.Command = new Command((obj) => {
+                Navigation.PopAsync();
+            });
+        }
+    }
+}
